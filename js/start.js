47f7208de79f3848210fef47f13d9498212d76cb
AntoineOgerETPA <AntoineOgerETPA@users.noreply.github.com>
2020-04-21 13:45:48 +0200

    Ressources principales

---
+++ js/start.js
@@ -0,0 +1,21 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: 'content',
+    width: 800,
+    height: 600,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false // set to true to view zones
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene,
+        Scene0
+    ]
+};
+var game = new Phaser.Game(config);
